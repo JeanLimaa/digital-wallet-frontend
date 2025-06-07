0153774831f5f904030d47dd45b7314d7ff6bf96
Jean Lima <jeansantoslima17@gmail.com>
2025-06-07 10:04:49 -0300

    fix(routes): update protected routes and default authenticated redirect to use '/wallet'

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,4 +1,4 @@
 export const PUBLIC_ROUTES = ['/auth/login', '/auth/register'];
-export const PROTECTED_ROUTES = ['/dashboard', '/transactions'];
-export const DEFAULT_AUTHENTICATED_REDIRECT = '/dashboard';
+export const PROTECTED_ROUTES = ['/wallet', '/transactions'];
+export const DEFAULT_AUTHENTICATED_REDIRECT = '/wallet';
 export const DEFAULT_UNAUTHENTICATED_REDIRECT = '/auth/login';
