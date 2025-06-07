f1e3fcd6cf11f8e9740f6e76c7beeb0bf708dea6
Jean Lima <jeansantoslima17@gmail.com>
2025-06-07 09:59:49 -0300

    feat(auth.schema): add authentication schemas for login and registration using Zod

---
+++ src/schemas/auth.schema.ts
@@ -0,0 +1,10 @@
+import z from "zod";
+
+export const loginSchema = z.object({
+  email: z.string({message: 'E-mail é obrigatorio'}).email({ message: 'E-mail inválido' }),
+  password: z.string().min(6, { message: 'Senha deve ter ao menos 6 caracteres' }),
+});
+
+export const registerSchema = loginSchema.extend({
+  name: z.string({message: 'Senha é obrigatorio'}).min(2, { message: 'Nome é obrigatório' }),
+});
